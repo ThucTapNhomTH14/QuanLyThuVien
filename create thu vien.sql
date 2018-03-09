cc96f4a45cd76b9ff628df58254e1d61224c06b7
phamson197 <phamson11999977@gmail.com>
2018-03-09 12:19:11 +0700

    Update create thu vien.sql

--- create thu vien.sql
+++ create thu vien.sql
@@ -2,21 +2,22 @@ use [quan ly thu vien]
 
 drop table if exists DauSach
 create table DauSach (
-Sach_ID varchar(20),
+Sach_ID varchar(20) not null primary key,
 Sach_ten nvarchar(40),
 )
 
 drop table if exists DocGia
 create table DocGia (
-DocGia_ID varchar(20),
+DocGia_ID varchar(20) not null primary key,
 DocGia_ten nvarchar(40),
 DocGia_ngaysinh date,
 )
 
 drop table if exists TTMuonTra
 create table TTMuonTra (
-Sach_ID varchar(20),
-DocGia_ID varchar(20),
+TTMuonTra_ID varchar(20) not null primary key,
+Sach_ID varchar(20) not null foreign key references DauSach(Sach_ID),
+DocGia_ID varchar(20) not null foreign key references DocGia(DocGia_ID),
 NgayMuon date,
 NgayTra date,
 )
