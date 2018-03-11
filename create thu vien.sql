d25c8d187ea15dd2e185ac28a6c7d184f51c4a53
phamson197 <phamson11999977@gmail.com>
2018-03-11 15:09:33 +0700

    Update create thu vien.sql

--- create thu vien.sql
+++ create thu vien.sql
@@ -7,9 +7,15 @@ TacGia_ten nvarchar(40)
 )
 drop table if exists DauSach
 create table DauSach (
-Sach_ID varchar(20) not null primary key,
+DauSach_ID varchar(20) not null primary key,
 Sach_ten nvarchar(40),
 TacGia_ID varchar(20)
+)
+
+drop table if exists Sach
+create table BanSao (
+Sach_ID varchar(20) not null primary key,
+DauSach_ID varchar(20) not null foreign key references DauSach(Sach_ID),
 )
 
 drop table if exists DocGia
@@ -22,7 +28,7 @@ DocGia_ngaysinh date,
 drop table if exists TTMuonTra
 create table TTMuonTra (
 TTMuonTra_ID varchar(20) not null primary key,
-Sach_ID varchar(20) not null foreign key references DauSach(Sach_ID),
+Sach_ID varchar(20) not null foreign key references Sach(Sach_ID),
 DocGia_ID varchar(20) not null foreign key references DocGia(DocGia_ID),
 NgayMuon date,
 NgayTra date,
