775b4591b698f6005bc5840bbdfa3ecbc1ce9dc9
phamson197 <phamson11999977@gmail.com>
2018-03-12 22:06:33 +0700

    Add files via upload

--- proc thu vien.sql
+++ proc thu vien.sql
@@ -17,11 +17,21 @@ begin
 update DocGia set DocGia_ten=@ten,DocGia_ngaysinh=@birth where @ID=DocGia_ID
 end
 
+drop proc if exists XoaDocGia
+go
+create proc XoaDocGia(@ID varchar(20)=null,@ten nvarchar(20)=null,@birth date=null)
+as
+begin
+delete from DocGia where @ID=DocGia_ID
+end
+
 drop proc if exists TimDocGia
 go
 create proc TimDocGia(@ID varchar(20)=null,@ten nvarchar(20)=null,@birth date=null)
 as
 begin
 select * from DocGia
-where @ID=DocGia_ID or (@ID=DocGia_ID and @ten=DocGia_ten) or (@ID=DocGia_ID and @ten=DocGia_ten and @birth=DocGia_ngaysinh)
+where @ID=DocGia_ID or
+(@ID=DocGia_ID and @ten=DocGia_ten) or
+(@ID=DocGia_ID and @ten=DocGia_ten and @birth=DocGia_ngaysinh)
 end
